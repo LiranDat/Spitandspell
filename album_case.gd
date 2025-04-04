0d6cf4260ff788b3053bb528679a64f78652e898
Liran <lirandattner@googlemail.com>
2025-04-04 16:43:24 +0200

    Change to album animation

--- album_case.gd
+++ album_case.gd
@@ -1,6 +1,7 @@
 class_name AlbumCase extends Node2D
 var time = 0.0
-var albums = []
+var albums = [null,null,null,null]
+var albumCount = 0
 const MAXSIZE = 4
 var full = false
 const positions = [Vector2(36,40),Vector2(108,40),Vector2(36,104),Vector2(108,104)]
@@ -40,20 +41,23 @@ func buyAlbum(album:Album):
 	if $Money.money >= album.price:
 		$Money.money -= album.price
 		addAlbum(album)
+		print(albums)
 		return true
+	print(albums)
 	return false
 	pass
 	
 func addAlbum(album : Album):
 	if(full):
 		return
-	albums.append(album)
+	albums[albumCount]=album
 	album.reparent($Albums)
 	var tween = album.create_tween()
 	tween.set_ease(Tween.EASE_IN_OUT)
 	tween.set_trans(Tween.TRANS_ELASTIC)
-	tween.tween_property(album,"position",positions[albums.size()-1],1.0)
-	if(albums.size() >= MAXSIZE):
+	tween.tween_property(album,"position",positions[albumCount],1.0)
+	albumCount += 1
+	if(albumCount >= MAXSIZE):
 		full = true
 	pass
 
@@ -62,19 +66,31 @@ func sellAlbum(album:Album):
 	removeAlbum(album)
 
 func removeAlbum(album : Album):
-	if(albums.find(album)>0):
-		print(albums.find(album))
-		albums[albums.find(album)]=null
+	for searchIndex in albums.size():
+		if(albums[searchIndex]==album):
+			albums[searchIndex]=null
+	albumCount -=1
 	full = false
-	var ownedAlbums = []
-	while(albums.size()>0):
-		if(albums[0]!=null):
-			ownedAlbums.push_back(albums[0])
-		albums.pop_front()
-	print(ownedAlbums.size())
-	albums.clear()
-	for ownedAlbum in ownedAlbums:
-		addAlbum(ownedAlbum)
+	var ownedAlbums = [null,null,null,null]
+	var index = 0
+	for slot in albums.size():
+		if(albums[slot] != null):
+			ownedAlbums[index]=albums[slot]
+			index+=1
+	print("owning "+ str(ownedAlbums.size())+" albums")
+	for slot in albums.size():
+		if(ownedAlbums[slot] != null):
+			print("moving album")
+			albums[slot]=ownedAlbums[slot]
+		else:
+			albums[slot]=null
+	for slot in albums.size():		
+		if(albums[slot]):
+			var tween = albums[slot].create_tween()
+			tween.set_ease(Tween.EASE_IN_OUT)
+			tween.set_trans(Tween.TRANS_ELASTIC)
+			tween.tween_property(albums[slot],"position",positions[slot],1.0)
+	print(albums)
 
 func addMoney(money:float):
 	$Money.money += money
