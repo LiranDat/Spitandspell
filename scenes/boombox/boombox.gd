1e815deb73d77def87b8e8375ccddd0eb83ee8bd
reyojne-aeki <jo8472@proton.me>
2025-04-04 03:31:04 +0200

    bessere texturen für boombox

--- scenes/boombox/boombox.gd
+++ scenes/boombox/boombox.gd
@@ -18,8 +18,8 @@ func bounce():
 	var tween1 = get_tree().create_tween()
 	var tween2 = get_tree().create_tween()
 	
-	tween2.tween_property($speaker_2, "scale", Vector2(2,2), 0.1).set_trans(Tween.TRANS_BOUNCE)
-	tween1.tween_property($speaker_1, "scale", Vector2(2,2), 0.1).set_trans(Tween.TRANS_BOUNCE)
+	tween2.tween_property($speaker_2, "scale", Vector2(1.5,1.5), 0.1).set_trans(Tween.TRANS_BOUNCE)
+	tween1.tween_property($speaker_1, "scale", Vector2(1.5,1.5), 0.1).set_trans(Tween.TRANS_BOUNCE)
 	
 	tween2.tween_property($speaker_2, "scale", Vector2(1,1), 2.2).set_trans(Tween.TRANS_BOUNCE)
 	tween1.tween_property($speaker_1, "scale", Vector2(1,1), 2.2).set_trans(Tween.TRANS_BOUNCE)
