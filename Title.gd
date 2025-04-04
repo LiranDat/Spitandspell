d283629375707569ea1e0f02b59a74d0d5feb4b6
Liran <lirandattner@googlemail.com>
2025-04-04 14:59:20 +0200

    Title screen added

--- Title.gd
+++ Title.gd
@@ -7,12 +7,12 @@ func _process(delta):
 func sway(delta):
 	var posValue = sin(time)*6.0
 	var rotValue = cos(time*.3)*1.0/(8.0*PI)
-	$Bar1.position.x = posValue
-	$Bar1.rotation = rotValue
-	$Bar2.position.x = -sin(time*.7)*6.0
-	$Bar2.rotation = -cos(time*.5)*1.0/(6.0*PI)
-	$Bar3.position.x = sin(time*.9)*8.0
-	$Bar3.rotation = -cos(time*.5)*1.0/(8.0*PI)
+	$Center/Bar1.position.x = posValue
+	$Center/Bar1.rotation = rotValue
+	$Center/Bar2.position.x = -sin(time*.7)*6.0
+	$Center/Bar2.rotation = -cos(time*.5)*1.0/(6.0*PI)
+	$Center/Bar3.position.x = sin(time*.9)*8.0
+	$Center/Bar3.rotation = -cos(time*.5)*1.0/(8.0*PI)
 	
 	time+=delta
 	pass
